8db10be46cf50d88e6e3c53336417bc0c6ae9768
Reiase <reiase@gmnail.com>
2026-09-06 10:44:20 +0800

    docs: use constellation artwork as the site background

--- docs/website/docusaurus.config.js
+++ docs/website/docusaurus.config.js
@@ -31,7 +31,7 @@ const config = {
     }],
   ],
   themeConfig: {
-    image: 'img/diagrams/persisting/zodiac-sky.svg',
+    image: 'img/diagrams/persisting/zodiac-figures.png',
     docs: {
       sidebar: {
         hideable: true,
